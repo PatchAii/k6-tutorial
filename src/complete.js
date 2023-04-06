ea9580a2586e8216f882f0427be1a57856e2b640
Andrea Quintino <andrea.quintino@alirahealth.com>
2023-04-06 11:20:06 +0200

    update complete examples

--- src/complete.js
+++ src/complete.js
@@ -4,28 +4,28 @@ import exec from "k6/execution";
 import http from 'k6/http';
 
 const data = new SharedArray("dataset", function () {
-  return JSON.parse(open('user-list.json'));
+    return JSON.parse(open('user-list.json'));
 })
 
 export let options = {
     scenarios: {
         "ramping-scenario": {
-          executor: 'ramping-vus',
-          stages: [
-            { duration: '20s', target: 50 },
-            { duration: '20s', target: 50 },
-            { duration: '20s', target: 100 },
-            { duration: '20s', target: 250 }, // spike
-            { duration: '20s', target: 50 },
-            { duration: '20s', target: 0 },
-          ],
+            executor: 'ramping-vus',
+            stages: [
+                { duration: '5s', target: 50 },
+                { duration: '5s', target: 50 },
+                { duration: '5s', target: 100 },
+                { duration: '5s', target: 250 }, // spike
+                { duration: '5s', target: 50 },
+                { duration: '5s', target: 0 },
+            ],
         }
     },
     thresholds: {
         //check login duration for investigator@example.com
         "http_req_duration{name: login, email: investigator@example.com}": ["avg<300"],
         "http_req_duration{name: get-data}": ["avg<300"],
-        "http_req_duration{name: post-data}": ["avg<300"],
+        "http_req_duration{name: post-form}": ["avg<300"],
         'checks{name: login}': ['rate>0.9'],
         'checks{name: post-form}': ['rate>0.9'],
         'checks{name: get-data}': ['rate>0.9'],
@@ -34,16 +34,19 @@ export let options = {
 
 const host = `http://k6-target`
 
-export default function() {
+export default function () {
     const credentials = data[exec.scenario.iterationInTest % data.length]
     //login with custom tag on checks and request
-    const loginResponse = http.get(`${host}/basic-auth/${credentials["email"]}/${credentials["password"]}`,{
+    const loginResponse = http.post(`${host}/anything`, JSON.stringify(credentials), {
+        headers: {
+            'Content-Type': "application/json"
+        },
         tags: {
             email: credentials["email"],
             name: "login"
         }
     });
-    check(loginResponse, { "status 200": (r) => r.status === 200 },{
+    check(loginResponse, { "status 200": (r) => r.status === 200 }, {
         email: credentials["email"],
         name: "login"
     })
@@ -55,22 +58,23 @@ export default function() {
             name: "get-data"
         }
     })
+    // console.log(JSON.parse(jsonResponse.body)["slideshow"] !== undefined )
     check(jsonResponse, {
-        "check body": (r) => { JSON.parse(r.body)["slideshow"] !== undefined },
+        "check body": (r) => JSON.parse(r.body)["slideshow"] !== undefined ,
         "status 200": (r) => r.status === 200
-    },{
+    }, {
         name: "get-data"
     })
     sleep(0.2)
 
     // submitting heavy form
     const delay = (Math.floor(Math.random() * 10))
-    const formResponse = http.post(`${host}/delay/${delay}`, {data: "some data"}, {
+    const formResponse = http.post(`${host}/delay/${delay}`, { data: "some data" }, {
         tags: {
             name: "post-form"
         }
     });
-    check(formResponse, { "status 200": (r) => r.status === 200 },{
+    check(formResponse, { "status 200": (r) => r.status === 200 }, {
         name: "post-form"
     })
 }
